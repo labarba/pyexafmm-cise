e4d7825d9db877a2ad18537a4222d4f06069fd31
Srinath Kailasa <srinathkailasa@gmail.com>
2022-02-11 18:05:00 +0000

    Add pitfalls section

---
+++ article/nested_function.py
@@ -0,0 +1,33 @@
+import numpy as np
+import numba
+import numba.core
+import numba.typed
+
+
+# Dictionary instance created at runtime
+results = numba.typed.Dict.empty(
+    key_type=numba.core.types.unicode_type,
+    value_type=numba.core.types.float64[:]
+)
+
+@numba.njit
+def nested(input, results):
+    # This function expects a
+    # typed dictionary as an
+    # argument to store results
+
+    def step1(a):
+        # first step of algorithm
+        # store results in dictionary
+        ...
+
+    def step2(a):
+        # second step of algorithm
+        # store results in dictionary
+        ...
+
+    # Interpreter doesn't know about
+    # the changes in the dictionary
+    # until Numba runtime hands back
+    # control
+    return results
