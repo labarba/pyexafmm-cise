81626e5f9403a25f203552ae695586b1ff2230e5
Lorena A. Barba <labarba@gwu.edu>
2023-03-13 14:29:24 -0400

    push auto-generated .py files

--- article/parallel_multithreading.py
+++ article/parallel_multithreading.py
@@ -1,13 +1,10 @@
+
 import numba
 import numpy as np
 
 @numba.njit(cache=True, parallel=True)
-def multithreading(A):
-    # consider an A as an nxn matrix
-    # This is a situation that can lead
-    # to thread oversubscription unless
-    # Numpy is configured to run single
-    # threaded.
-    for _ in numba.prange(10):
-         B = A @ A
+def multithreading(a):
+
+ for i in numba.prange(10):
+  a[i] *= 10
 
